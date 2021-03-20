89139df1cfb8a94088e567ec1d607dc85f059c1c
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-03-20 08:55:47 +0600

    implement google authentication system

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from '../../Firebase/firebase.config';
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
 
 const Login = () => {
+
+    const [user, setUser] = useState({});
+
+    const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+    const handleGoogleSignIn = () => {
+        firebase.auth()
+            .signInWithPopup(googleProvider)
+            .then((result) => {
+                var credential = result.credential;
+                var token = credential.accessToken;
+                var user = result.user;
+                console.log(token, user)
+                setUser(user);
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                var email = error.email;
+                var credential = error.credential;
+                console.log(errorCode, errorMessage, email, credential);
+            });
+    }
+
     return (
         <div>
             <h1>This is Login Page</h1>
+            <button onClick={handleGoogleSignIn}>Sign in using Google</button>
+            <h3>{user.displayName}</h3>
         </div>
     );
 };
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCNAowHpz7cIhPgi3ogW-q2lVzwI1YwvrM",
+    authDomain: "dream-rider-info.firebaseapp.com",
+    projectId: "dream-rider-info",
+    storageBucket: "dream-rider-info.appspot.com",
+    messagingSenderId: "294155810230",
+    appId: "1:294155810230:web:c03bc5ff8dd1102d407d17"
+};
+
+export default firebaseConfig;
